d95e31dcb72cef1b1b12e948cff6bd1b64fa7abe
bmcminn <labs@gbox.name>
2015-07-12 21:32:33 -0500

    updating this thing to potentially use strtok instead of regex

--- test/test.php
+++ test/test.php
@@ -9,7 +9,12 @@
   // RUN TEST
   $testfile = file_get_contents(__DIR__.DS.'test.md');
 
-  file_put_contents(__DIR__.DS.'test-render.json', json_encode($smackdown->render($testfile),JSON_PRETTY_PRINT));
-  file_put_contents(__DIR__.DS.'test-renderfile.json', json_encode($smackdown->render($testfile),JSON_PRETTY_PRINT));
+  // file_put_contents(__DIR__.DS.'test-render.json', json_encode($smackdown->render($testfile),JSON_PRETTY_PRINT));
+  // file_put_contents(__DIR__.DS.'test-renderfile.json', json_encode($smackdown->render($testfile),JSON_PRETTY_PRINT));
 
-  print_r($smackdown);
+  // print_r($smackdown);
+
+
+  print_r(strtok($testfile));
+
+  // http://onlinephpguide.com/php-strtok
